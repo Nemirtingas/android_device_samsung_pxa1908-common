e50383ef166fcca29bb00f97c1808404cf8d818c
Nemirtingas <9432037+Nemirtingas@users.noreply.github.com>
2018-10-26 12:05:47 +0200

    Update BoardConfigCommon.mk
    Added some Marvell paths

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -73,16 +73,19 @@ BOARD_NEEDS_CUTILS_LOG := true
 BOARD_USES_HWCOMPOSER := true
 
 # Wifi
-BOARD_WLAN_VENDOR := MRVL
-MRVL_WIRELESS_DAEMON_API := true
+#BOARD_WLAN_VENDOR := MRVL
+#MRVL_WIRELESS_DAEMON_API := true
 #BOARD_WLAN_DEVICE                := qcwcn
-BOARD_HOSTAPD_DRIVER             := NL80211
+#BOARD_HOSTAPD_DRIVER             := NL80211
 #BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+#BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 #BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 #TARGET_PROVIDES_WCNSS_QMI        := true
 #TARGET_USES_QCOM_WCNSS_QMI       := true
 #TARGET_USES_WCNSS_CTRL           := true
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/mrvl/sd8777_uapsta.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/mrvl/sd8777_uapsta.bin"
+#WPA_SUPPLICANT_VERSION           := VER_0_8_X
+#WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/mrvl/sd8777_uapsta.bin"
+#WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/mrvl/sd8777_uapsta.bin"
+#WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/firmware/mrvl/sd8777_uapsta.bin"
+#WIFI_DRIVER_FW_PATH_PARAM	      := "/sys/module/wlan/parameters/fwpath"
+#WIFI_DRIVER_NVRAM_PATH_PARAM     := "/sys/module/dhd/parameters/nvram_path"
