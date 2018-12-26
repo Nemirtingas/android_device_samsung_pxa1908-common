d932b25b96f125fbe35b619b9b15a7cb67ac449f
Nemirtingas <nanaki89@hotmail.fr>
2018-12-26 08:59:35 +0100

    Took the MSM8226 power module.

--- power/power.c
+++ power/power.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,241 +13,214 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
 #define LOG_TAG "PowerHAL"
-#include <utils/Log.h>
 
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
-#define POWERHALCFG "/etc/powerhal.conf"
+#include <stdbool.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
 
-#define __AFILE__ "power.c"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-struct power_cpu_info
+#include <utils/Log.h>
+
+#include "power.h"
+
+/* touchkeys */
+#define TK_POWER "/sys/class/input/input1/enabled"
+/* touchscreen */
+#define TS_POWER "/sys/class/input/input2/enabled"
+
+#define CPUFREQ_PATH "/sys/devices/system/cpu/cpu0/cpufreq/"
+#define INTERACTIVE_PATH "/sys/devices/system/cpu/cpufreq/interactive/"
+
+static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+static int boostpulse_fd = -1;
+
+static int current_power_profile = -1;
+static int requested_power_profile = -1;
+
+static int sysfs_write_str(char *path, char *s)
 {
-	char path[256];
-	char runtime_path[256];
-	int  control_fd;
-	struct power_cpu_info *next;
-	struct power_cpu_info *prev;
-};
+    char buf[80];
+    int len;
+    int ret = 0;
+    int fd;
 
-static struct power_cpu_info *devices = NULL;
-static struct power_cpu_info *last_device = NULL;
+    fd = open(path, O_WRONLY);
+    if (fd < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error opening %s: %s\n", path, buf);
+        return -1 ;
+    }
 
-int add_to_list( const char* buffer, int control_fd )
-{
-	struct power_cpu_info *device;
-	struct power_cpu_info *old_last_device;
+    len = write(fd, s, strlen(s));
+    if (len < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error writing to %s: %s\n", path, buf);
+        ret = -1;
+    }
 
-	device = (struct power_cpu_info*)malloc(sizeof(struct power_cpu_info));
-	if( device )
-	{
-		memset(device, 0, sizeof(struct power_cpu_info));
-		strncpy(device->path, buffer, 256);
-		strncpy(device->runtime_path, buffer, 256);
-		strncat(device->runtime_path, "/runtime_status", 256);
-		device->control_fd = control_fd;
-		if( devices )
-		{
-			old_last_device = last_device;
-			last_device->next = device;
-			last_device = device;
-			last_device->prev = old_last_device;
-		}
-		else
-		{
-			last_device = device;
-			devices = device;
-		}
+    close(fd);
 
-		return 0;
-	}
-	else
-	{
-		ALOGE("malloc failed for creating new node");
-	}
-	return -1;
+    return ret;
 }
 
-int is_pm_supported( const char *control, const char *runtime )
+static int sysfs_write_int(char *path, int value)
 {
-	char buf[12];
-	int fd;
-	if( access(control, 0) < 0 )
-	{
-		ALOGW("%s doesn't exist\n", control);
-		return -1;
-	}
-	fd = open(runtime, 0);
-	if( fd < 0 )
-	{
-		ALOGW("Error opening %s: %s\n", runtime, strerror(errno));
-		return -1;
-	}
-	if( read(fd, buf, sizeof(buf)) < 0 )
-	{
-		ALOGW("Error reading %s: %s\n", runtime, strerror(errno));
-		close(fd);
-		return -1;
-	}
-	if( !strncmp(buf, "unsupported", 11) )
-	{
-		ALOGW("%s is unsupported\n", runtime);
-		close(fd);
-		return -1;
-	}
-	close(fd);
-	return 0;
+    char buf[80];
+    snprintf(buf, 80, "%d", value);
+    return sysfs_write_str(path, buf);
 }
 
-void pm_set_for_device( struct power_cpu_info *device, int on )
+static bool check_governor(void)
 {
-	if( on )
-	{
-		if( write(device->control_fd, "on", 2) < 0 )
-		{
-			ALOGE("Error writing %s to %s: %s\n", "on", device->path, strerror(errno));
-			return;
-		}
-		ALOGD("write on done\n");
-		return;
-	}
-	if( write(device->control_fd, "auto", 4) >= 0 )
-		ALOGD("write auto done\n");
-	else
-		ALOGE("Error writing %s to %s: %s\n", "auto", device->path, strerror(errno));
+    struct stat s;
+    int err = stat(INTERACTIVE_PATH, &s);
+    if (err != 0) return false;
+    if (S_ISDIR(s.st_mode)) return true;
+    return false;
 }
 
-void wait_for_set_complete(const char* runtime_path, int on )
+static int is_profile_valid(int profile)
 {
-	int fd;
-	char buf[12];
-	int retry = 1000;
-	const char *state;
-	int state_len;
-
-	while( retry-- > 0 )
-	{
-		fd = open(runtime_path, 0);
-		if( fd < 0 )
-		{
-			ALOGE("Error opening %s: %s\n", runtime_path, strerror(errno));
-			return;
-		}
-		if( read(fd, buf, sizeof(buf)) < 0 )
-		{
-			close(fd);
-			ALOGE("Error reading %s: %s\n", runtime_path, strerror(errno));
-		}
-		else
-		{
-			close(fd);
-			if( !strncmp(buf, "unsupported", 11) )
-			{
-				ALOGE("%s is unsupported\n", runtime_path);
-				break;
-			}
-			if( on )
-			{
-				state = "active";
-				state_len = 6;
-			}
-			else
-			{
-				state = "suspended";
-				state_len = 9;
-			}
-			if( !strncmp(buf, state, state_len) )
-				break;
-
-			usleep(1000);
-		}
-	}
-	if( retry <= 0 )
-		ALOGW("DEVICE SUSPEND/RESUME FAILED!\n");
-	ALOGD("wait_for_set_complete done, status is %s", buf);
+    return profile >= 0 && profile < PROFILE_MAX;
 }
 
 static void power_init(__attribute__((unused)) struct power_module *module)
 {
-	FILE *conf = fopen(POWERHALCFG, "r");
-	int fd;
-	char buffer[256];
-	char control[256];
-	char runtime[256];
-	if( !conf )
-	{
-		ALOGW("%s does not exist! No devices for early suspend/late resume", POWERHALCFG);
-		return;
-	}
-	while( fgets(buffer, 256, conf) )
-	{
-		memset(control, 0, sizeof(control));
-		if( buffer[0] != '#' && buffer[0] != '\n' && buffer[0] != ' ' )
-		{
-			buffer[strlen(buffer)-1] = '\0';
-			strncpy(control, buffer, 256);
-			strncat(control, "/control", 256);
-			strncpy(runtime, buffer, 256);
-			strncat(runtime, "/runtime_status", 256);
-			if( is_pm_supported(control, runtime) )
-			{
-				ALOGE("device %s doesn't support pm\n", buffer);
-				continue;
-			}
-			fd = open(control, 2);
-			if( fd >= 0 )
-			{
-				ALOGD("add device %s to list\n", buffer);
-				if( add_to_list(buffer, fd) )
-				{
-					ALOGE("add device %s to list failed\n", buffer);
-				}
-			}
-			else
-			{
-				ALOGE("Error opening %s: %s\n", control, strerror(errno));
-			}
-		}
-	}
-	fclose(conf);
+    ALOGI("%s", __func__);
+}
+
+static int boostpulse_open()
+{
+    pthread_mutex_lock(&lock);
+    if (boostpulse_fd < 0) {
+        boostpulse_fd = open(INTERACTIVE_PATH "boostpulse", O_WRONLY);
+    }
+    pthread_mutex_unlock(&lock);
+
+    return boostpulse_fd;
+}
+
+static void power_set_interactive_ext(int on) {
+    ALOGD("%s: %s input devices", __func__, on ? "enabling" : "disabling");
+    sysfs_write_str(TK_POWER, on ? "1" : "0");
+    sysfs_write_str(TS_POWER, on ? "1" : "0");
 }
 
 static void power_set_interactive(__attribute__((unused)) struct power_module *module, int on)
 {
-	struct power_cpu_info *i;
+    if (!is_profile_valid(current_power_profile)) {
+        ALOGD("%s: no power profile selected yet", __func__);
+        return;
+    }
 
-	ALOGD("power_set_interactive called with on %d\n", on);
-	if( on )
-	{
-		for( i = last_device; i; i = i->prev )
-		{
-			pm_set_for_device(i, on);
-		}
-	}
-	else
-	{
-		for( i = devices; i; i = i->next )
-		{
-			pm_set_for_device(i, 0);
-		}
-	}
-	for( i = last_device; i; i = i->prev )
-	{
-		wait_for_set_complete(i->runtime_path, on);
-	}
+    power_set_interactive_ext(on);
+
+    // break out early if governor is not interactive
+    if (!check_governor()) return;
+
+    if (on) {
+        sysfs_write_int(INTERACTIVE_PATH "hispeed_freq",
+                        profiles[current_power_profile].hispeed_freq);
+        sysfs_write_int(INTERACTIVE_PATH "go_hispeed_load",
+                        profiles[current_power_profile].go_hispeed_load);
+        sysfs_write_str(INTERACTIVE_PATH "target_loads",
+                        profiles[current_power_profile].target_loads);
+    } else {
+        sysfs_write_int(INTERACTIVE_PATH "hispeed_freq",
+                        profiles[current_power_profile].hispeed_freq_off);
+        sysfs_write_int(INTERACTIVE_PATH "go_hispeed_load",
+                        profiles[current_power_profile].go_hispeed_load_off);
+        sysfs_write_str(INTERACTIVE_PATH "target_loads",
+                        profiles[current_power_profile].target_loads_off);
+    }
 }
 
-static void power_hint(__attribute__((unused)) struct power_module *module, power_hint_t hint,
-                       __attribute__((unused)) void *data) {
+static void set_power_profile(int profile)
+{
+    if (!is_profile_valid(profile)) {
+        ALOGE("%s: unknown profile: %d", __func__, profile);
+        return;
+    }
+
+    // break out early if governor is not interactive
+    if (!check_governor()) return;
+
+    if (profile == current_power_profile)
+        return;
+
+    ALOGD("%s: setting profile %d", __func__, profile);
+
+    sysfs_write_int(INTERACTIVE_PATH "boost",
+                    profiles[profile].boost);
+    sysfs_write_int(INTERACTIVE_PATH "boostpulse_duration",
+                    profiles[profile].boostpulse_duration);
+    sysfs_write_int(INTERACTIVE_PATH "go_hispeed_load",
+                    profiles[profile].go_hispeed_load);
+    sysfs_write_int(INTERACTIVE_PATH "hispeed_freq",
+                    profiles[profile].hispeed_freq);
+    sysfs_write_int(INTERACTIVE_PATH "io_is_busy",
+                    profiles[profile].io_is_busy);
+    sysfs_write_int(INTERACTIVE_PATH "min_sample_time",
+                    profiles[profile].min_sample_time);
+    sysfs_write_int(INTERACTIVE_PATH "sampling_down_factor",
+                    profiles[profile].sampling_down_factor);
+    sysfs_write_str(INTERACTIVE_PATH "target_loads",
+                    profiles[profile].target_loads);
+    sysfs_write_int(CPUFREQ_PATH "scaling_max_freq",
+                    profiles[profile].scaling_max_freq);
+
+    current_power_profile = profile;
+}
+
+static void power_hint(__attribute__((unused)) struct power_module *module,
+                       power_hint_t hint, void *data)
+{
+    char buf[80];
+    int len;
+
     switch (hint) {
+    case POWER_HINT_INTERACTION:
+        if (!is_profile_valid(current_power_profile)) {
+            ALOGD("%s: no power profile selected yet", __func__);
+            return;
+        }
+
+        if (!profiles[current_power_profile].boostpulse_duration)
+            return;
+
+        // break out early if governor is not interactive
+        if (!check_governor()) return;
+
+        if (boostpulse_open() >= 0) {
+            snprintf(buf, sizeof(buf), "%d", 1);
+            len = write(boostpulse_fd, &buf, sizeof(buf));
+            if (len < 0) {
+                strerror_r(errno, buf, sizeof(buf));
+                ALOGE("Error writing to boostpulse: %s\n", buf);
+
+                pthread_mutex_lock(&lock);
+                close(boostpulse_fd);
+                boostpulse_fd = -1;
+                pthread_mutex_unlock(&lock);
+            }
+        }
+        break;
+    case POWER_HINT_SET_PROFILE:
+        pthread_mutex_lock(&lock);
+        set_power_profile(*(int32_t *)data);
+        pthread_mutex_unlock(&lock);
+        break;
+    case POWER_HINT_LOW_POWER:
+        /* This hint is handled by the framework */
+        break;
     default:
         break;
     }
@@ -257,28 +230,28 @@ static struct hw_module_methods_t power_module_methods = {
     .open = NULL,
 };
 
+static int get_feature(__attribute__((unused)) struct power_module *module,
+                       feature_t feature)
+{
+    if (feature == POWER_FEATURE_SUPPORTED_PROFILES) {
+        return PROFILE_MAX;
+    }
+    return -1;
+}
+
 struct power_module HAL_MODULE_INFO_SYM = {
     .common = {
         .tag = HARDWARE_MODULE_TAG,
         .module_api_version = POWER_MODULE_API_VERSION_0_2,
         .hal_api_version = HARDWARE_HAL_API_VERSION,
         .id = POWER_HARDWARE_MODULE_ID,
-        .name = "Marvell Power HAL",
-        .author = "Nemirtingas",
+        .name = "msm8226 Power HAL",
+        .author = "Gabriele M",
         .methods = &power_module_methods,
     },
 
     .init = power_init,
     .setInteractive = power_set_interactive,
     .powerHint = power_hint,
+    .getFeature = get_feature
 };
-
-__attribute__((constructor)) void __module_load__()
-{
-	ALOGI("%s loaded.", "power.mrvl.so");
-}
-
-__attribute__((destructor)) void __module_unload__()
-{
-	ALOGI("%s unloaded.", "power.mrvl.so");
-}
